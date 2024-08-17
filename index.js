253bfc1b29f18ca344b8b6e8d8b67e9bde504697
Luka Piersma <luka.piersma@hva.nl>
2024-08-18 01:19:07 +0200

    database

--- index.js
+++ index.js
@@ -19,4 +19,35 @@ client.on('messageCreate', (message) => {
 
 client.login(process.env.TOKEN);
 
-console.log('haha!')
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: 'db-buf-05.sparkedhost.us',
+    port: 3306, // Default port for MySQL
+    user: 'u143949_Wv5pkUs1q6',
+    password: 'sxN86zmnsvC+gRVGnz+lvTKx',
+    database: 's143949_puppy-bot'
+});
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+// Example query
+connection.query('SELECT * FROM `User`', (err, results, fields) => {
+    if (err) {
+        console.error('Error executing query:', err.stack);
+        return;
+    }
+    console.log('Query results:', results);
+});
+
+// Close the connection when done
+// You should manage connection pooling and closing based on your application's needs
+connection.end();
